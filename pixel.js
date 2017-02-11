64d8e9ba5f5df9b381ba7f9890f55b5faa22c8ae
144mdgross <144.mdgross@gmail.com>
2017-02-11 14:14:40 -0700

    the palete is working. still a simple grid. basic skeleton.

--- pixel.js
+++ pixel.js
@@ -6,7 +6,12 @@ var theBody = document.getElementsByTagName('body')[0]
 console.log(theBody)
 
 var paintDiv = document.createElement('div')
+paintDiv.classList.add('canvas')
 theBody.appendChild(paintDiv)
+
+var palateDiv = document.createElement('div')
+palateDiv.classList.add('palate')
+theBody.appendChild(palateDiv)
 
 //make div to attach other divs to
 for (var j = 0; j < 10; j++) {
@@ -23,22 +28,21 @@ for(var i = 0; i < 10; i++) {
 }
 }
 
-//create gap to distinguish pallette from grid
-var gap = document.createElement('div')
-gap.classList.add('gap')
-theBody.appendChild(gap)
-console.log('gap', gap)
+//create gap to distinguish palate from grid
+// var gap = document.createElement('div')
+// gap.classList.add('gap')
+// theBody.appendChild(gap)
+// console.log('gap', gap)
 
-//create pallette rows
-for (var j = 0; j < 2; j++) {
+//create palate rows
   var colorDivRow = document.createElement('div')
   colorDivRow.classList.add('grid')
-  theBody.appendChild(colorDivRow)
-//create pallette columns
+  palateDiv.appendChild(colorDivRow)
+//create palate columns
 //now get them to access colors stored in array
-var colorArray = ['red', 'green', 'purple']
+var colorArray = ['red', 'green', 'purple', 'pink', 'yellow', 'maroon']
 //this gives me as many divs as elements in the color array. but the color follows the columns instead of being unique to each square. fix later.
-//make color pallette with an array
+//make color palate with an array
 for (var w = 0; w < colorArray.length; w++) {
     var colorDivColumn = document.createElement('div')
     var styleColor = colorArray[w]
@@ -48,24 +52,66 @@ for (var w = 0; w < colorArray.length; w++) {
   colorDivRow.appendChild(colorDivColumn)
   }
 
-}
 
-//add eventListener
-theBody.addEventListener("click", function(e){
-  e.target.style.backgroundColor = "red";
-//define function to share color between what's click on the pallette and the grid.
+
+//add reset button.
+var reset = document.createElement('button')
+reset.classList.add('reset-button')
+reset.innerText = 'reset'
+palateDiv.appendChild(reset)
+console.log('reset', reset)
+
+//add eventListener for changing color
+paintDiv.addEventListener("mousedown", function(e){
+//keep the drag from highlighting the whole body
+  if (e.target === this) {
+    //do nothing
+  } else {
+    //do what i want you to do
+    e.target.style.backgroundColor = activeColor['color'];
+  }
+//define function to share color between what's click on the palate and the grid.
 
 })//click the body change the backgroundColor to red listener
 //create event listener and function to return class of pallette clicked
+var activeColor = {
+  color: 'crimson',
+}
+//function to save palete color. should it be a method?
+
+
 
 //put event listener on the pallette..
+palateDiv.addEventListener('click', function(e){
+console.log('clicked on palete')
+//this is working. now how to update the color object when clicking on targets?
+color = e.target.style.backgroundColor
 
+function chooseColor (color) {
+  if (color !== activeColor['color']) {
+      activeColor['color'] = color
+  }
+  return activeColor['color']
+  console.log(activeColor['color'])
+}
+
+chooseColor(color)
+
+
+})//end of palete event listener
 
 // gap.addEventListener("click", function(e){
 //   //adding event listener to gap does not add it to the color pallette. Maybe make separate divs for the main sections so i don't have to have the clicked listener on just the body.
 //   e.target.style.backgroundColor = "yellow"
 //   console.log('clicked grid @ ' + e)
 // }) //click color pallette listener end
+
+//add reset event listener
+reset.addEventListener('click', function (e){
+
+
+})//end of reset even listener
+
 
 console.log(theBody)
 
