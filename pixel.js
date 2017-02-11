ad78884919ed6cd6c788f6a40473d75ada81ac73
144mdgross <144.mdgross@gmail.com>
2017-02-11 16:54:42 -0700

    mouseover creating color. won't start and stop though.

--- pixel.js
+++ pixel.js
@@ -1,4 +1,5 @@
 //add COMCONTENTLOADED
+
 document.addEventListener("DOMContentLoaded", function (event){
 
 //select the body
@@ -14,14 +15,14 @@ palateDiv.classList.add('palate')
 theBody.appendChild(palateDiv)
 
 //make div to attach other divs to
-for (var j = 0; j < 10; j++) {
+for (var j = 0; j < 20; j++) {
 var theDiv = document.createElement('div')
 theDiv.classList.add('grid')
 paintDiv.appendChild(theDiv)
 console.log(theDiv)
 
 //make other divs and attach them to the grid div
-for(var i = 0; i < 10; i++) {
+for(var i = 0; i < 100; i++) {
   var theOtherDiv = document.createElement('div')
   theOtherDiv.classList.add('column')
   theDiv.appendChild(theOtherDiv)
@@ -40,7 +41,10 @@ for(var i = 0; i < 10; i++) {
   palateDiv.appendChild(colorDivRow)
 //create palate columns
 //now get them to access colors stored in array
-var colorArray = ['red', 'green', 'purple', 'pink', 'yellow', 'maroon']
+//var colorArray = ['red', 'green', 'purple', 'pink', 'yellow', 'maroon']
+var colorArray = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"];
+
+
 //this gives me as many divs as elements in the color array. but the color follows the columns instead of being unique to each square. fix later.
 //make color palate with an array
 for (var w = 0; w < colorArray.length; w++) {
@@ -76,12 +80,33 @@ paintDiv.addEventListener("mousedown", function(e){
   } else {
     //do what i want you to do
     e.target.style.backgroundColor = activeColor['color'];
+    paintDiv.addEventListener('mouseover', function(e){
+      console.log('mouseover')
+      if (e.target === this) {
+        //do nothing
+      } else {
+      e.target.style.backgroundColor = activeColor['color']
+      paintDiv.addEventListener('mouseup', function(e){
+        if (e.target === this) {
+          //do nothing
+        } else {
+          console.log('mouseup')
+          e.target.style.backgroundColor = 'white'
+          //do nothing
+        }
+
+      })//end of mouseup
+      }
+  })//end of mouseover
     //alter function for dragging mouse
   }
 //define function to share color between what's click on the palate and the grid.
 
 })//click the body change the backgroundColor to red listener
 //create event listener and function to return class of pallette clicked
+
+
+
 var activeColor = {
   color: 'black',
 }
@@ -93,15 +118,19 @@ var palateClicks = {
   count: 0
 }
 
+//declare function so I can remove event listener? is that what I want to do to only activate one thing? Can I use a pseudo class instead?
+
 //put event listener on the pallette..
 palateDiv.addEventListener('click', function(e){
 console.log('clicked on palete')
 //this is working. now how to update the color object when clicking on targets?
-if (e.target) {
+
 palateClicks['count'] += 1
-}
+//how to add and remove box shadow?
 console.log('palate clicks ==>', palateClicks['count'])
 e.target.style.boxShadow = "0px 0px 30px blue"
+//e.target.classList.add('shadow')
+console.log(e.target.classList)
 color = e.target.style.backgroundColor
 
 function chooseColor (color) {
